3de63cf08f24b7c64a33037681c9180017f873e1
Canaan <cjmckenz@uvm.edu>
2022-03-16 11:45:02 -0400

    moving on

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ApplicationRecord
 
+	belongs_to :user
 	#add validation to enforce twitter 140 char
-	validates :content, length: {maximum: 140}
+	validates :content, length: {maximum: 140},
+						presence: true
 end
